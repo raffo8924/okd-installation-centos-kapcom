ce0dc2298bed99dafc1b1458c16bc721c73cae57
Javier Raffo <raffo8924@gmail.com>
2019-07-19 12:57:44 -0500

    Update settings.sh

--- provisioning/settings.sh
+++ provisioning/settings.sh
@@ -29,7 +29,7 @@ export OKD_INFRA_NODE_1_HOSTNAME=okd-infra-node-1
 #By default we can login using the URL https://console.okd.nip.io:8443
 #To access URL from your local system we need to configure master host in C:\Windows\System32\drivers\etc\hosts file as below
 #192.168.1.100  console.okd.nip.io
-export DOMAIN=okd.nip.io
+export DOMAIN=kapcomperu.nip.io
 export API_PORT=8443
 
 #OKD Login Credentials
@@ -39,7 +39,7 @@ export OKD_PASSWORD=k@pc0m.2018
 
 #OKD Add-Ons
 #Enable "True"  only if one of the VM has 4GB memory.
-export INSTALL_METRICS=True
+export INSTALL_METRICS=true
 
 # Enable "True"  only one of the VM 16GB memory.
-export INSTALL_LOGGING=False
+export INSTALL_LOGGING=false
